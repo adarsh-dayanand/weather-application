0f8cd5c43cd49d25b4c7c11e03322295aad58a6f
adarsh-dayanand <ad.29.1999@gmail.com>
2020-03-18 15:06:18 +0530

    Mobile View

--- src/app.js
+++ src/app.js
@@ -69,33 +69,10 @@ app.get('/weather', (req, res) => {
     })
 })
 
-/*
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
-
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Andrew Mead',
-        errorMessage: 'Help article not found.'
-    })
-})
-*/
-
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Adarsh',
         errorMessage: 'Page not found.'
     })
 })
